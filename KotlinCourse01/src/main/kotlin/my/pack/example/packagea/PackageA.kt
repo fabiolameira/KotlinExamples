21c77fa9b0f383652e30fcc9463b7e9e8ee51463
Fábio Lameira <fabiolameira97@gmail.com>
2021-09-12 01:29:03 +0100

    Adding packages example

---
+++ KotlinCourse01/src/main/kotlin/my/pack/example/packagea/PackageA.kt
@@ -0,0 +1,3 @@
+package my.pack.example.packagea
+
+fun name(): String = "Package A"
---
+++ KotlinCourse01/src/main/kotlin/my/pack/example/packageb/PackageB.kt
@@ -0,0 +1,7 @@
+package my.pack.example.packageb
+
+import my.pack.example.packagea.name
+
+fun main() {
+    println(name())
+}
