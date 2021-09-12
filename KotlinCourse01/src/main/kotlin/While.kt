2899fd2631fba6b85342eb9f403ae863c99b607c
Fábio Lameira <fabiolameira97@gmail.com>
2021-09-12 18:50:40 +0100

    Change while to do while

--- KotlinCourse01/src/main/kotlin/While.kt
+++ KotlinCourse01/src/main/kotlin/While.kt
@@ -1,9 +1,9 @@
 fun main() {
-    var temperature: Int = 0
+    var temperature: Int
     var summation: Int = 0
     var counter: Int = 0
 
-    while (temperature != 999) {
+    do {
         println("Insert a temperature or 999 to exit:")
         temperature = readLine()!!.toInt()
 
@@ -11,7 +11,7 @@ fun main() {
             summation += temperature
             counter++
         }
-    }
+    } while (temperature != 999)
 
     println("The median temperature its: ${summation / counter} ºC")
 }
