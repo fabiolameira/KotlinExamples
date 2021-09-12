5dc9975dcada0d17a48b7394c42deb98d2c15090
Fábio Lameira <fabiolameira97@gmail.com>
2021-09-12 15:49:00 +0100

    Refactoring getResult function to use when

--- KotlinCourse01/src/main/kotlin/SmartCast.kt
+++ KotlinCourse01/src/main/kotlin/SmartCast.kt
@@ -1,17 +1,16 @@
+import kotlin.math.exp
+
 interface Expression
 
 class Number(val value: Int): Expression
 class Sum(val first: Expression, val second: Expression): Expression
 
-fun getResult(expression: Expression) : Int {
-    if (expression is Number)
-        return expression.value
-
-    if (expression is Sum)
-        return getResult(expression.first) + getResult(expression.second)
-
-    throw IllegalArgumentException("Invalid Expression")
-}
+fun getResult(expression: Expression) : Int =
+    when(expression) {
+        is Number -> expression.value
+        is Sum -> getResult(expression.first) + getResult(expression.second)
+        else -> throw IllegalArgumentException("Invalid expression!")
+    }
 
 fun main() {
     println("((1 + 5) + 5) = ${getResult(Sum(Sum(Number(1), Number(5)), Number(5)))}")
